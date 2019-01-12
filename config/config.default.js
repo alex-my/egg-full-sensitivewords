42724c5d6430d23e838d910ec831cf252e4a48a5
alex <alex_my@126.com>
2019-01-12 11:12:35 +0800

    创建工程

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-full-sensitivewords default config
+ * @member Config#fullSensitivewords
+ * @property {String} SOME_KEY - some description
+ */
+exports.fullSensitivewords = {
+
+};
